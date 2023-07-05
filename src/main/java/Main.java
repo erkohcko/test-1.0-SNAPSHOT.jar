d4eeb5a5f7ce0a819e010bc0513111bd91a45f0e
Erkochko <wenarun@yandex.ru>
2023-07-05 09:03:40 +0300

    переделал, не разобрался

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,22 @@
+import lombok.*;
+
 public class Main {
     public static void main(String[] args) {
-    Human human = new Human();
-    human.firstName = "Andrey";
-    human.lastName = "Petrov";
+        Human human = new Human();
+        human.setFirstName("Andrey");
+        human.setLastName("Petrov");
         System.out.println(human);
     }
-    public static class Human {
-        private String firstName;
-        private String lastName;
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        @Override
-        public String toString() {
-            return super.toString();
-        }
-        public Human() {
-
-        }
-        public Human (String firstName, String lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
+}
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+class Human {
+    private String firstName;
+    private String lastName;
 
 
-    }
+
 }
